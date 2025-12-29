997b0a4c90004f91bb98bcd237c6f3c44fc50106
actor <gizatullininsaf25@gmail.com>
2025-12-29 04:33:34 +0300

    fix issue with symlink and fzf

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -63,43 +63,67 @@ return {
                     border = "rounded",
 
                     draw = {
-                        components = {
-                            kind_icon = {
-                                text = function(ctx)
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local mini_icon, _ = require("mini.icons").get_icon(ctx.item.data.type, ctx
-                                            .label)
-                                        if mini_icon then return mini_icon .. ctx.icon_gap end
-                                    end
-
-                                    local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
-                                    return icon .. ctx.icon_gap
-                                end,
-
-                                -- Optionally, use the highlight groups from mini.icons
-                                -- You can also add the same function for `kind.highlight` if you want to
-                                -- keep the highlight groups in sync with the icons.
-                                highlight = function(ctx)
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local mini_icon, mini_hl = require("mini.icons").get_icon(ctx.item.data.type,
-                                            ctx.label)
-                                        if mini_icon then return mini_hl end
-                                    end
-                                    return ctx.kind_hl
-                                end,
+                        completion = {
+                            -- list = {
+                            -- 	preselect = true,
+                            -- 	-- auto_insert = true,
+                            -- },
+                            ghost_text = {
+                                enabled = false,
                             },
-                            kind = {
-                                -- Optional, use highlights from mini.icons
-                                highlight = function(ctx)
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local mini_icon, mini_hl = require("mini.icons").get_icon(ctx.item.data.type,
-                                            ctx.label)
-                                        if mini_icon then return mini_hl end
-                                    end
-                                    return ctx.kind_hl
-                                end,
-                            }
-                        }
+                            documentation = { auto_show = true, auto_show_delay_ms = 50 },
+                            -- documentation = { window = { border = "single" } },
+                            menu = {
+                                auto_show = true,
+                                border = "rounded",
+
+                                -- nvim-cmp style menu
+                                draw = {
+                                    columns = {
+                                        { "label",     "label_description", gap = 2 },
+                                        { "kind_icon", "kind",              gap = 2 },
+                                    },
+                                },
+                            },
+                        },
+
+                        -- components = {
+                        --     kind_icon = {
+                        --         text = function(ctx)
+                        --             if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                        --                 local mini_icon, _ = require("mini.icons").get_icon(ctx.item.data.type, ctx
+                        --                     .label)
+                        --                 if mini_icon then return mini_icon .. ctx.icon_gap end
+                        --             end
+                        --
+                        --             local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
+                        --             return icon .. ctx.icon_gap
+                        --         end,
+                        --
+                        --         -- Optionally, use the highlight groups from mini.icons
+                        --         -- You can also add the same function for `kind.highlight` if you want to
+                        --         -- keep the highlight groups in sync with the icons.
+                        --         highlight = function(ctx)
+                        --             if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                        --                 local mini_icon, mini_hl = require("mini.icons").get_icon(ctx.item.data.type,
+                        --                     ctx.label)
+                        --                 if mini_icon then return mini_hl end
+                        --             end
+                        --             return ctx.kind_hl
+                        --         end,
+                        --     },
+                        --     kind = {
+                        --         -- Optional, use highlights from mini.icons
+                        --         highlight = function(ctx)
+                        --             if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                        --                 local mini_icon, mini_hl = require("mini.icons").get_icon(ctx.item.data.type,
+                        --                     ctx.label)
+                        --                 if mini_icon then return mini_hl end
+                        --             end
+                        --             return ctx.kind_hl
+                        --         end,
+                        --     }
+                        -- }
                     }
                 },
             },
